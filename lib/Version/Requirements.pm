8d0dc924c8813ca57912336929d1333efc727822
Ricardo SIGNES <rjbs@cpan.org>
2010-02-20 14:29:51 -0500

    major overhaul; handles META.yml 1.4 spec

--- lib/Version/Requirements.pm
+++ lib/Version/Requirements.pm
@@ -3,6 +3,7 @@ use warnings;
 package Version::Requirements;
 # ABSTRACT: a set of version requirements for a CPAN dist
 
+use Carp ();
 use Scalar::Util ();
 use version ();
 
@@ -11,34 +12,202 @@ sub new {
   return bless {} => $class;
 }
 
-sub add_minimum {
-  my ($self, $name, $version) = @_;
+sub _version_object {
+  my ($self, $version) = @_;
 
-  $version = version->parse($version)
-    if defined $version and ! Scalar::Util::blessed($version);
+  $version = (! defined $version)                ? version->parse(0)
+           : (! Scalar::Util::blessed($version)) ? version->parse($version)
+           :                                       $version;
 
-  if (defined (my $oldver = $self->{ $name })) {
-    if (defined $version) {
-      $self->{ $name } = (sort { $b cmp $a } ($version, $oldver))[0];
-    }
-    return;
+  return $version;
+}
+
+BEGIN {
+  for my $type (qw(minimum maximum exclusion)) {
+    my $method = "with_$type";
+    my $code = sub {
+      my ($self, $name, $version) = @_;
+
+      $version = $self->_version_object( $version );
+
+      my $old = $self->{ $name } || 'Version::Requirements::_Spec::Range';
+
+      $self->{ $name } = $old->$method($version);
+    };
+    
+    my $to_add = "add_$type";
+    no strict 'refs';
+    *$to_add = $code;
+  }
+}
+
+sub __modules   { keys %{ $_[ 0 ] } }
+sub __entry_for { $_[0]{ $_[1] }    }
+
+sub as_string_hash {
+  my ($self) = @_;
+
+  my %hash;
+  for my $module ($self->__modules) {
+    $hash{ $module } = $self->__entry_for($module)->as_string;
   }
 
-  $self->{ $name } = $version;
+  return \%hash;
 }
 
-sub minimums {
-  my ($self) = @_;
-  return { %$self };
+sub __x_meets_req_y {
+  my ($self, $given, $req) = @_;
+
+  # We have a specific requirement:
+  my ($type, $spec) = @$req;
+
+  if ($type eq 'exact') {
+    return $given == $spec;
+  } elsif ($type eq 'range') {
+  }
+
+  Carp::croak "unknown version requirement type: $type";
 }
 
-sub minimum_strings {
-  my ($self) = @_;
+# Module => {
+#   minimum => x,
+#   maximum => y,
+#   exclude => z,
+# }
+# Module => exact_vesion
 
-  my %return = map {; $_ => (defined $self->{$_} ? "$self->{$_}" : undef) }
-               keys %$self;
+sub add_maximum;
+sub add_exclusion;
 
-  return \%return;
+sub requirements { die'...' }
+
+{
+  package
+    Version::Requirements::_Spec::Exact;
+  sub _new     { bless { version => $_[1] } => $_[0] }
+  sub _accepts { return $_[0]{version} == $_[1] }
+
+  sub as_string { return "== $_[0]{version}" }
+
+  sub with_minimum {
+    my ($self, $minimum) = @_;
+    return $self if $self->{version} >= $minimum;
+    Carp::confess("illegal requirements: minimum above exact specification");
+  }
+
+  sub with_maximum {
+    my ($self, $maximum) = @_;
+    return $self if $self->{version} <= $maximum;
+    Carp::confess("illegal requirements: maximum below exact specification");
+  }
+
+  sub with_exclusion {
+    my ($self, $exclusion) = @_;
+    return $self unless $exclusion == $self;
+    Carp::confess("illegal requirements: excluded exact specification");
+  }
+}
+
+{
+  package
+    Version::Requirements::_Spec::Range;
+
+  sub _self { ref($_[0]) ? $_[0] : (bless { } => $_[0]) }
+
+  sub as_string {
+    my ($self) = @_;
+
+    return 0 if ! keys %$self;
+
+    return "$self->{minimum}" if (keys %$self) == 1 and exists $self->{minimum};
+
+    my @parts;
+    push @parts, ">= $self->{minimum}" if exists $self->{minimum};
+    push @parts, "<= $self->{maximum}" if exists $self->{maximum};
+    push @parts, map {; "!= $_" } @{ $self->{exclusions} || [] };
+
+    return join q{, }, @parts;
+  }
+
+  sub _simplify {
+    my ($self) = @_;
+
+    if (defined $self->{minimum} and defined $self->{maximum}) {
+      if ($self->{minimum} == $self->{maximum}) {
+        Carp::confess("illegal requirements: excluded all values")
+          if grep { $_ == $self->{minimum} } @{ $self->{exclusions} || [] };
+
+        return Version::Requirements::_Spec::Exact->_new($self->{minimum})
+      }
+
+      Carp::confess("illegal requirements: minimum exceeds maximum")
+        if $self->{minimum} > $self->{maximum};
+    }
+
+    # eliminate irrelevant exclusions
+    if ($self->{exclusions}) {
+      my %seen;
+      @{ $self->{exclusions} } = grep {
+        (! defined $self->{minimum} or $_ >= $self->{minimum})
+        and
+        (! defined $self->{maximum} or $_ <= $self->{maximum})
+        and
+        ! $seen{$_}++
+      } @{ $self->{exclusions} };
+    }
+
+    return $self;
+  }
+
+  sub with_minimum {
+    my ($self, $minimum) = @_;
+    $self = $self->_self;
+
+    # If $minimum is false, it's undef or 0, which cannot be meaningful as a
+    # minimum.  -- rjbs, 2010-02-20
+    return $self unless $minimum;
+
+    if (defined (my $old_min = $self->{minimum})) {
+      $self->{minimum} = (sort { $b cmp $a } ($minimum, $old_min))[0];
+    } else {
+      $self->{minimum} = $minimum;
+    }
+
+    return $self->_simplify;
+  }
+
+  sub with_maximum {
+    my ($self, $maximum) = @_;
+    $self = $self->_self;
+
+    if (defined (my $old_max = $self->{maximum})) {
+      $self->{maximum} = (sort { $a cmp $b } ($maximum, $old_max))[0];
+    } else {
+      $self->{maximum} = $maximum;
+    }
+
+    return $self->_simplify;
+  }
+
+  sub with_exclusion {
+    my ($self, $exclusion) = @_;
+    $self = $self->_self;
+
+    push @{ $self->{exclusions} ||= [] }, $exclusion;
+
+    return $self->_simplify;
+  }
+
+  sub _accepts {
+    my ($self, $version) = @_;
+
+    return if defined $self->{minimum} and $version < $self->{minimum};
+    return if defined $self->{maximum} and $version > $self->{maximum};
+    return if defined $self->{exclusions}
+          and grep { $version == $_ } @{ $self->{exclusions} };
+
+    return 1;
+  }
 }
 
 1;
--- t/basic.t
+++ t/basic.t
@@ -6,44 +6,106 @@ use version;
 
 use Test::More 0.88;
 
-my $req = Version::Requirements->new;
+{
+  my $req = Version::Requirements->new;
 
-$req->add_minimum('Foo::Bar' => 10);
-$req->add_minimum('Foo::Bar' => undef);
-$req->add_minimum('Foo::Bar' => 2);
+  $req->add_minimum('Foo::Bar' => 10);
+  $req->add_minimum('Foo::Bar' => 0);
+  $req->add_minimum('Foo::Bar' => 2);
 
-$req->add_minimum('Foo::Baz' => version->declare('v1.2.3'));
+  $req->add_minimum('Foo::Baz' => version->declare('v1.2.3'));
 
-$req->add_minimum('Foo::Undef' => undef);
+  $req->add_minimum('Foo::Undef' => 0);
 
-is_deeply(
-  $req->minimums,
-  {
-    'Foo::Bar'   => version->declare(10),
-    'Foo::Baz'   => version->declare('v1.2.3'),
-    'Foo::Undef' => undef,
-  },
-  "some basic minimums",
-);
+  is_deeply(
+    $req->as_string_hash,
+    {
+      'Foo::Bar'   => 10,
+      'Foo::Baz'   => 'v1.2.3',
+      'Foo::Undef' => 0,
+    },
+    "some basic minimums",
+  );
+}
 
-is_deeply(
-  $req->minimum_strings,
-  {
-    'Foo::Bar'   => 10,
-    'Foo::Baz'   => 'v1.2.3',
-    'Foo::Undef' => undef,
-  },
-  "some basic minimums",
-);
+{
+  my $req = Version::Requirements->new;
 
-ok(
-  ( ref $req->minimums->{'Foo::Bar'}),
-  '->minimums returns version objects',
-);
+  $req->add_minimum(Foo => 1);
+  $req->add_maximum(Foo => 2);
 
-ok(
-  ( ! ref $req->minimum_strings->{'Foo::Bar'}),
-  '->minimum_strings returns strings',
-);
+  is_deeply(
+    $req->as_string_hash,
+    {
+      Foo => '>= 1, <= 2',
+    },
+    "min and max",
+  );
+
+  $req->add_maximum(Foo => 3);
+
+  is_deeply(
+    $req->as_string_hash,
+    {
+      Foo => '>= 1, <= 2',
+    },
+    "exclusions already outside range do not matter",
+  );
+
+  $req->add_exclusion(Foo => 1.5);
+
+  is_deeply(
+    $req->as_string_hash,
+    {
+      Foo => '>= 1, <= 2, != 1.5',
+    },
+    "exclusions",
+  );
+
+  $req->add_minimum(Foo => 1.6);
+
+  is_deeply(
+    $req->as_string_hash,
+    {
+      Foo => '>= 1.6, <= 2',
+    },
+    "exclusions go away when made irrelevant",
+  );
+}
+
+{
+  # ATTENTION
+  # This might change in the future to generate '> 1, <= 2'
+  # but there is no need to.  Just do not rely on it too much.
+  my $req = Version::Requirements->new;
+
+  $req->add_minimum(Foo => 1);
+  $req->add_exclusion(Foo => 1);
+  $req->add_maximum(Foo => 2);
+
+  is_deeply(
+    $req->as_string_hash,
+    {
+      Foo => '>= 1, <= 2, != 1',
+    },
+    "we can exclude an endpoint",
+  );
+}
+
+{
+  my $req = Version::Requirements->new;
+
+  my $ok = eval {
+    $req->add_minimum(Foo => 1);
+    $req->add_exclusion(Foo => 1);
+    $req->add_maximum(Foo => 1);
+    1;
+  };
+
+  my $error = $@;
+
+  ok(!$ok, "we can't exclude all values")
+    or diag explain $req->as_string_hash;
+}
 
 done_testing;
