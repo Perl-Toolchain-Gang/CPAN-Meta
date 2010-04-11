f849531a081ca6164842d63a1b7234a6e1eb7c62
Ricardo Signes <rjbs@cpan.org>
2010-04-11 14:02:55 -0400

    make sure our lists are set a compile time

--- lib/CPAN/Meta.pm
+++ lib/CPAN/Meta.pm
@@ -5,30 +5,30 @@ package CPAN::Meta;
 use Carp qw(confess);
 use CPAN::Meta::Prereqs;
 
-my @STRING_READERS = qw(
-  abstract
-  description
-  dynamic_config
-  generated_by
-  name
-  release_status
-  version
-);
-
 BEGIN {
+  my @STRING_READERS = qw(
+    abstract
+    description
+    dynamic_config
+    generated_by
+    name
+    release_status
+    version
+  );
+
   no strict 'refs';
   for my $attr (@STRING_READERS) {
     *$attr = sub { $_[0]{ $attr } };
   }
 }
 
-my @LIST_READERS = qw(
-  author
-  keywords
-  license
-);
-
 BEGIN {
+  my @LIST_READERS = qw(
+    author
+    keywords
+    license
+  );
+
   no strict 'refs';
   for my $attr (@LIST_READERS) {
     *$attr = sub {
@@ -39,25 +39,35 @@ BEGIN {
   }
 }
 
-my @MAP_READERS = qw(
-  meta_spec
-  resources
-  provides
-  no_index
-
-  prereqs
-  optional_features
-);
+sub authors  { $_[0]->author }
+sub licenses { $_[0]->license }
 
 BEGIN {
+  my @MAP_READERS = qw(
+    meta-spec
+    resources
+    provides
+    no_index
+
+    prereqs
+    optional_features
+  );
+
   no strict 'refs';
   for my $attr (@MAP_READERS) {
-    *$attr = sub {
+    (my $subname = $attr) =~ s/-/_/;
+    *$subname = sub {
       my $value = $_[0]{ $attr };
       return $value if $value;
       return {};
     };
   }
+}
+
+sub new {
+  my ($class, $struct) = @_;
+
+  bless $struct => $class;
 }
 
 sub meta_spec_version {
