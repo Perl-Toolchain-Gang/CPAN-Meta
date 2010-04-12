d7343eec731b9a959274bd338547151395f4627c
Ricardo Signes <rjbs@cpan.org>
2010-04-11 20:35:45 -0400

    documentation for CPAN::Meta

--- lib/CPAN/Meta.pm
+++ lib/CPAN/Meta.pm
@@ -1,10 +1,64 @@
 use strict;
 use warnings;
 package CPAN::Meta;
+# ABSTRACT: the distribution metadata for a CPAN dist
+
+=head1 SYNOPSIS
+
+  my $struct = decode_json_file('META.json');
+
+  my $meta = CPAN::Meta->new($struct);
+
+  printf "testing requirements for %s version %s\n",
+    $meta->name,
+    $meta->version;
+
+  my $prereqs = $meta->requirements_for('configure');
+
+  for my $module ($prereqs->required_modules) {
+    my $version = get_local_version($module);
+
+    die "missing required module $module" unless defined $version;
+    die "version for $module not in range"
+      unless $prereqs->accepts_module($module, $version);
+  }
+
+=head1 DESCRIPTION
+
+Software distributions released to the CPAN include a F<META.json> or, for
+older distributions, F<META.yml>, which describes the distribution, its
+contents, and the requirements for building and installing the distribution.
+The data structure stored in the F<META.json> file is described in
+L<CPAN::Meta::Spec>.
+
+CPAN::Meta provides a simple class to represent this distribution metadata (or
+I<distmeta>), along with some helpful methods for interrogating that data.
+
+The documentation below is only for the methods of the CPAN::Meta object.  For
+information on the meaning of individual fields, consult the spec.
+
+=cut
 
 use Carp qw(confess);
 use CPAN::Meta::Feature;
 use CPAN::Meta::Prereqs;
+
+=head1 STRING DATA
+
+The following methods return a single value, which is the value for the
+corresponding entry in the distmeta structure.  Values should be either undef
+or strings.
+
+=for :list
+* abstract
+* description
+* dynamic_config
+* generated_by
+* name
+* release_status
+* version
+
+=cut
 
 BEGIN {
   my @STRING_READERS = qw(
@@ -22,6 +76,21 @@ BEGIN {
     *$attr = sub { $_[0]{ $attr } };
   }
 }
+
+=head1 LIST DATA
+
+These methods return lists of string values, which might be represented in the
+distmeta structure as arrayrefs or scalars:
+
+=for :list
+* authors
+* keywords
+* licenses
+
+The C<authors> and C<licenses> methods may also be called as C<author> and
+C<license>, respectively, to match the field name in the distmeta structure.
+
+=cut
 
 BEGIN {
   my @LIST_READERS = qw(
@@ -45,6 +114,21 @@ BEGIN {
 sub authors  { $_[0]->author }
 sub licenses { $_[0]->license }
 
+=head1 MAP DATA
+
+These readers return hashrefs of arbitrary unblessed data structures, each
+described more fully in the specification:
+
+=for :list
+* meta_spec
+* resources
+* provides
+* no_index
+* prereqs
+* optional_features
+
+=cut
+
 BEGIN {
   my @MAP_READERS = qw(
     meta-spec
@@ -67,16 +151,44 @@ BEGIN {
   }
 }
 
+=method new
+
+  my $meta = CPAN::Meta->new($distmeta_struct);
+
+=cut
+
 sub new {
   my ($class, $struct) = @_;
 
   bless $struct => $class;
 }
 
+=method meta_spec_version
+
+This method returns the version part of the C<meta_spec> entry in the distmeta
+structure.  It is equivalent to:
+
+  $meta->meta_spec->{version};
+
+=cut
+
 sub meta_spec_version {
   my ($self) = @_;
   return $self->meta_spec->{version};
 }
+
+=method effective_prereqs
+
+  my $prereqs = $meta->effective_prereqs;
+
+  my $prereqs = $meta->effective_prereqs( \@feature_identifiers );
+
+This method returns a L<CPAN::Meta::Prereqs> object describing all the
+prereqs for the distribution.  If an arrayref of feature identifiers is given,
+the prereqs for the identified features are merged together with the
+distribution's core prereqs before the CPAN::Meta::Prereqs object is returned.
+
+=cut
 
 sub effective_prereqs {
   my ($self, $features) = @_;
@@ -86,14 +198,22 @@ sub effective_prereqs {
 
   return $prereq unless @$features;
 
-  my @other = map {;
-    confess "unknown feature requested: $_"
-      unless my $f = $self->optional_features->{$_};
-    CPAN::Meta::Prereqs->new($f->{prereqs});
-  } @$features;
+  my @other = map {; $self->feature($_)->prereqs } @$features;
 
   return $prereq->with_merged_prereqs(\@other);
 }
+
+=method should_index_file
+
+  ... if $meta->should_index_file( $filename );
+
+This method returns true if the given file should be indexed.  It decides this
+by checking the C<file> and C<directory> keys in the C<no_index> property of
+the distmeta structure.
+
+C<$filename> should be given in unix format.
+
+=cut
 
 sub should_index_file {
   my ($self, $filename) = @_;
@@ -110,6 +230,16 @@ sub should_index_file {
   return 1;
 }
 
+=method should_index_package
+
+  ... if $meta->should_index_package( $package );
+
+This method returns true if the given package should be indexed.  It decides
+this by checking the C<package> and C<namespace> keys in the C<no_index>
+property of the distmeta structure.
+
+=cut
+
 sub should_index_package {
   my ($self, $package) = @_;
 
@@ -124,6 +254,15 @@ sub should_index_package {
   return 1;
 }
 
+=method features
+
+  my @feature_objects = $meta->features;
+
+This method returns a list of L<CPAN::Meta::Feature> objects, one for each
+optional feature described by the distribution's metadata.
+
+=cut
+
 sub features {
   my ($self) = @_;
 
@@ -133,6 +272,16 @@ sub features {
 
   return @features;
 }
+
+=method features
+
+  my $feature_object = $meta->feature( $identifier );
+
+This method returns a L<CPAN::Meta::Feature> object for the optional feature
+with the given identifier.  If no feature with that identifier exists, an
+exception will be raised.
+
+=cut
 
 sub feature {
   my ($self, $ident) = @_;
