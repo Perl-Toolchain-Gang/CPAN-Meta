e529040b7c11a4a4ae9f1139541a8ca1b638c4a4
David Golden <dagolden@cpan.org>
2010-04-12 09:29:05 -0400

    first (partial) draft of Converter.pm

---
+++ lib/CPAN/Meta/Converter.pm
@@ -0,0 +1,150 @@
+use strict;
+use warnings;
+package CPAN::Meta::Converter;
+# ABSTRACT: Convert CPAN distribution metadata structures
+
+use Carp qw(carp confess);
+
+my %known_specs = (
+    '2'   => 'http://search.cpan.org/perldoc?CPAN::Meta::Spec',
+    '1.4' => 'http://module-build.sourceforge.net/META-spec-v1.4.html',
+    '1.3' => 'http://module-build.sourceforge.net/META-spec-v1.3.html',
+    '1.2' => 'http://module-build.sourceforge.net/META-spec-v1.2.html',
+    '1.1' => 'http://module-build.sourceforge.net/META-spec-v1.1.html',
+    '1.0' => 'http://module-build.sourceforge.net/META-spec-v1.0.html'
+);
+
+
+#--------------------------------------------------------------------------#
+# converters
+#
+# called as $converter->($element, $field_name, $full_meta, $to_version)
+# 
+# defined return value used for field
+# undef return value means field is skipped 
+#--------------------------------------------------------------------------#
+
+sub _keep { $_[0] }
+
+sub _keep_or_one { defined($_[0]) ? $_[0] : 1 }
+
+sub _generated_by { __PACKAGE__ . " version " . __PACKAGE__->VERSION }
+
+sub _listify { ref $_[0] eq 'ARRAY' ? $_[0] : [$_[0]] }
+
+sub _prefix_custom { "x_" . $_[0] }
+
+sub _change_meta_spec {
+  my ($element, undef, undef, $version) = @_;
+  $element->{version} = $version;
+  $element->{url} = $known_specs{$version};
+  return $element;
+}
+
+sub _prereqs {
+  my (undef, undef, $meta) = @_;
+  my $prereqs = {};
+  for my $phase ( qw/build configure/ ) {
+    my $key = "${phase}_requires";
+    $prereqs->{$phase}{requires} = $meta->{$key} if $meta->{$key};
+  }
+  for my $rel ( qw/requires recommends conflicts/ ) {
+    $prereqs->{runtime}{$rel} = $meta->{$rel} if $meta->{$rel};
+  }
+  return $prereqs;
+}
+
+sub _optional_features_2 {
+  my (undef, undef, $meta) = @_;
+  return undef unless exists $meta->{optional_features};
+  my $origin = $meta->{optional_features};
+  my $features = {};
+  for my $name ( keys %$origin ) {
+    $features->{$name} = {
+      description => $origin->{$name}{description},
+      prereqs => _prereqs->(undef, undef, $origin->{$name}),
+    };
+    delete $features->{$name}{prereqs}{configure};
+  } 
+  return $features;
+}
+  
+#  resources => {
+#    license     => [ 'http://dev.perl.org/licenses/' ],
+#    homepage    => 'http://sourceforge.net/projects/module-build',
+#    bugtracker  => {
+#      web    => 'http://github.com/dagolden/cpan-meta-spec/issues',
+#      mailto => 'meta-bugs@example.com',
+#    },
+#    repository  => {
+#      url  => 'git://github.com/dagolden/cpan-meta-spec.git',
+#      web  => 'http://github.com/dagolden/cpan-meta-spec',
+#      type => 'git',
+#    },
+
+my $resource_conversion_spec = {
+  license    => \&_listify,
+  homepage   => \&_keep,
+  bugtracker => sub { return { web => $_[0] } },
+  repository => sub { return { web => $_[0] } },
+  ':custom'  => \&_prefix_custom,
+};
+
+sub _resources_2 {
+  my (undef, undef, $meta) = @_;
+  return undef unless exists $meta->{resources};
+  return _convert($meta->{resource}, $resource_conversion_spec);
+}
+
+#--------------------------------------------------------------------------#
+# define converters for each conversion
+#--------------------------------------------------------------------------#
+
+# each converts from prior version
+# special ":custom" field is used for keys not recognized in spec
+my %up_convert = (
+  '2-from-1.4' => {
+    'meta-spec'           => \&_change_meta_spec,
+    'name'                => \&_keep,
+    'version'             => \&_keep,
+    'abstract'            => \&_keep,
+    'author'              => \&_listify,
+    'license'             => \&_listify,
+    'generated_by'        => \&_generated_by,
+    'dynamic_config'      => \&_keep_or_one,
+    'prereqs'             => \&_prereqs,
+    'optional_features'   => \&_optional_features_2,
+    'provides'            => \&_keep,
+    'no_index'            => \&_keep,
+    'keywords'            => \&_keep,
+    'resources'           => \&_resources_2,
+
+    # drop these deprecated fields, but only after we convert
+    ':drop' => [ qw/ private distribution_type / ],
+
+    # other random keys need x_ prefixing
+    ':custom'              => \&prefix_custom,
+  },
+);
+
+
+sub new {
+  my ($class,$data) = @_;
+
+  # create an attributes hash
+  my $self = {
+    'data'    => $data,
+    'spec'    => $data->{'meta-spec'}{'version'} || "1.0",
+  };
+
+  # create the object
+  return bless $self, $class;
+}
+
+sub convert_to {
+  my ($self, $to_version) = @_;
+
+  die "unimplemented";
+}
+
+1;
