98821c02866fb00388df19fd4e650bcd6e6142a6
Ricardo Signes <rjbs@cpan.org>
2010-04-11 21:39:25 -0400

    documentation for Prereqs

--- lib/CPAN/Meta/Prereqs.pm
+++ lib/CPAN/Meta/Prereqs.pm
@@ -15,6 +15,34 @@ use Carp qw(confess);
 use Scalar::Util qw(blessed);
 use Version::Requirements 0.101010; # accepts_module
 
+=method new
+
+  my $prereq = CPAN::Meta::Prereqs->new( \%prereq_spec );
+
+This method returns a new set of Prereqs.  The input should look like the
+contents of the C<prereqs> field described in L<CPAN::Meta::Spec>, meaning
+something more or less like this:
+
+  my $prereq = CPAN::Meta::Prereqs->new({
+    runtime => {
+      requires => {
+        'Some::Module' => '1.234',
+        ...,
+      },
+      ...,
+    },
+    ...,
+  });
+
+You can also construct an empty set of prereqs with:
+
+  my $prereqs = CPAN::Meta::Prereqs->new;
+
+This empty set of prereqs is useful for accumulating new prereqs before finally
+dumping the whole set into a structure or string.
+
+=cut
+
 sub __legal_phases { qw(configure build test runtime develop)   }
 sub __legal_types  { qw(requires recommends suggests conflicts) }
 
@@ -47,8 +75,25 @@ sub new {
   return bless \%guts => $class;
 }
 
+=method requirements_for
+
+  my $requirements = $prereqs->requirements_for( $phase, $type );
+
+This method returns a L<Version::Requirements> object for the given phase/type
+combination.  If no prerequisites are registered for that combination, a new
+Version::Requirements object will be returned, and it may be added to as
+needed.
+
+If C<$phase> or C<$type> are undefined or otherwise invalid, an exception will
+be raised.
+
+=cut
+
 sub requirements_for {
   my ($self, $phase, $type) = @_;
+
+  confess "requirements_for called without phase" unless defined $phase;
+  confess "requirements_for called without type"  unless defined $type;
 
   my $req = Version::Requirements->new;
 
@@ -62,6 +107,22 @@ sub requirements_for {
 
   return $self->{ $phase }{ $type } ||= Version::Requirements->new;
 }
+
+=method with_merged_prereqs
+
+  my $new_prereqs = $prereqs->with_merged_prereqs( $other_prereqs );
+
+  my $new_prereqs = $prereqs->with_merged_prereqs( \@other_prereqs );
+
+This method returns a new CPAN::Meta::Prereqs objects in which all the
+other prerequisites given are merged into the current set.  This is primarily
+provided for combining a distribution's core prereqs with the prereqs of one of
+its optional features.
+
+The new prereqs object has no ties to the originals, and altering it further
+will not alter them.
+
+=cut
 
 sub with_merged_prereqs {
   my ($self, $other) = @_;
@@ -91,6 +152,14 @@ sub with_merged_prereqs {
 
   return (ref $self)->new(\%new_arg);
 }
+
+=method as_string_hash
+
+This method returns a hashref containing structures suitable for dumping into a
+distmeta data structure.  It is made up of hashes and strings, only; there will
+be no Prereqs, Version::Requirements, or C<version> objects inside it.
+
+=cut
 
 sub as_string_hash {
   my ($self) = @_;
