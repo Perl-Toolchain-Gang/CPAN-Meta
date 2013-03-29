Perl-Toolchain-Gang/CPAN-Meta

commit 4bdbc93d9db6d618373bf33a48d909165ddf9f9f

don't add version to provides if it didn't exist

lib/CPAN/Meta/Converter.pm
@@ -324,7 +324,7 @@ sub _is_module_name {
 }
 
 sub _clean_version {
-  my ($element, $key, $meta, $to_version) = @_;
+  my ($element) = @_;
   return 0 if ! defined $element;
 
   $element =~ s{^\s*}{};
@@ -693,12 +693,10 @@ sub _release_status_from_version {
 
 my $provides_spec = {
   file => \&_keep,
-  version => \&_clean_version,
 };
 
 my $provides_spec_2 = {
   file => \&_keep,
-  version => \&_clean_version,
   ':custom'  => \&_prefix_custom,
 };
 
@@ -709,6 +707,8 @@ sub _provides {
   my $new_data = {};
   for my $k ( keys %$element ) {
     $new_data->{$k} = _convert($element->{$k}, $spec, $to_version);
+    $new_data->{$k}{version} = _clean_version($element->{$k}{version})
+      if exists $element->{$k}{version};
   }
   return $new_data;
 }

t/converter.t
@@ -225,6 +225,18 @@ for my $f ( reverse sort @files ) {
   is( $converted->{prereqs}{runtime}{requires}{'File::Path'}, "v1.0.0", "normalize v1.0.0");
 }
 
+# specific test for missing provides version
+{
+  my $path = File::Spec->catfile('t','data','provides-version-missing.json');
+  my $original = Parse::CPAN::Meta->load_file( $path  );
+  ok( $original, "loaded " . basename $path );
+  my $cmc = CPAN::Meta::Converter->new( $original );
+  my $converted = $cmc->convert( version => 2 );
+  ok( exists $converted->{provides}{"Foo::Bar::Blah"}{file}, "found provides file" );
+  ok( ! exists $converted->{provides}{"Foo::Bar::Blah"}{version}, "missing version not added" )
+    or diag explain $converted;
+}
+
 # CMR standardizes stuff in a way that makes it hard to test original vs final
 # so we remove spaces and >= to make them compare the same
 sub _normalize_reqs {

commit 18dc70f66dbeba463cc5ef3479ea7c59d3e7e052

Preserve version attributes in provides hash
Commit 4bdbc93d9db6d618373bf33a48d909165ddf9f9f caused existing
'version' attributes to be renamed 'x_version'.

lib/CPAN/Meta/Converter.pm
@@ -693,10 +693,12 @@ sub _release_status_from_version {
 
 my $provides_spec = {
   file => \&_keep,
+  version => \&_keep,
 };
 
 my $provides_spec_2 = {
   file => \&_keep,
+  version => \&_keep,
   ':custom'  => \&_prefix_custom,
 };
 