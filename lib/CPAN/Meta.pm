800a15dc63ac36e71159c9b49e339605fabb35f2
David Golden <dagolden@cpan.org>
2013-06-05 12:49:16 -0400

    replace SYNOPSIS with code that works

--- lib/CPAN/Meta.pm
+++ lib/CPAN/Meta.pm
@@ -6,21 +6,35 @@ package CPAN::Meta;
 
 =head1 SYNOPSIS
 
-  my $meta = CPAN::Meta->load_file('META.json');
+    use v5.10;
+    use strict;
+    use warnings;
+    use CPAN::Meta;
+    use Module::Load;
 
-  printf "testing requirements for %s version %s\n",
+    my $meta = CPAN::Meta->load_file('META.json');
+
+    printf "testing requirements for %s version %s\n",
     $meta->name,
     $meta->version;
 
-  my $prereqs = $meta->requirements_for('configure');
+    my $prereqs = $meta->effective_prereqs;
 
-  for my $module ($prereqs->required_modules) {
-    my $version = get_local_version($module);
-
-    die "missing required module $module" unless defined $version;
-    die "version for $module not in range"
-      unless $prereqs->accepts_module($module, $version);
-  }
+    for my $phase ( qw/configure runtime build test/ ) {
+        say "Requirements for $phase:";
+        my $reqs = $prereqs->requirements_for($phase, "requires");
+        for my $module ( sort $reqs->required_modules ) {
+            my $status;
+            if ( eval { load $module unless $module eq 'perl'; 1 } ) {
+                my $version = $module eq 'perl' ? $] : $module->VERSION;
+                $status = $reqs->accepts_module($module, $version)
+                        ? "$version ok" : "$version not ok";
+            } else {
+                $status = "missing"
+            };
+            say "  $module ($status)";
+        }
+    }
 
 =head1 DESCRIPTION
 
