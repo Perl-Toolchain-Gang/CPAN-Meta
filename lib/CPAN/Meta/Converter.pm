9bc9899abc2a9879ef4f74dce72107706f5415ae
Kenichi Ishigaki <ishigaki@cpan.org>
2013-10-06 13:38:31 +0900

    make sure $meta->{'meta-spec'}{version} returns a valid version
    - as it dies abruptly when $meta->{'meta-spec'} illegally returns a scalar spec version (example: JOSEPHW/XML-Writer-0.600.tar.gz)

--- lib/CPAN/Meta/Converter.pm
+++ lib/CPAN/Meta/Converter.pm
@@ -1226,7 +1226,7 @@ sub new {
   # create an attributes hash
   my $self = {
     'data'    => $data,
-    'spec'    => $data->{'meta-spec'}{'version'} || "1.0",
+    'spec'    => eval { $data->{'meta-spec'}{'version'} || "1.0" } || 0,
   };
 
   # create the object
